d596f4bce92ba66d445ef08cf88c9d8249e1b647
im-not-enemy <opkd0710@gmail.com>
2020-09-05 21:21:37 +0900

    editer用API追加

---
+++ src/Adapter/Database.ts
@@ -0,0 +1,13 @@
+import nedb from 'nedb'
+import bluebird from 'bluebird'
+const db = new nedb({filename: './database/database.db', autoload: true})
+const Cursor = db.find({}).constructor
+// https://github.com/louischatriot/nedb/issues/276#issuecomment-225511866
+bluebird.promisifyAll(nedb.prototype)
+bluebird.promisifyAll(Cursor.prototype)
+
+export default class Database{
+    public insert(newDoc:{[key:string]:any}):void{
+        db.insert(newDoc)
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import Database from './Adapter/Database'
 
 const app = express()
 const port = "3000"
+
+const db = new Database()
 
 app.use(express.json())
 app.use(cors())
@@ -11,4 +14,10 @@ app.listen(port, () => {})
 app.get('/',(req,res)=>{
     res.send('Hello world!')
     console.log('helllo world!')
+})
+
+app.post('/menu',(req,res)=>{
+    const abc = req.body.abc
+    db.insert({abc:abc})
+    console.log(`insert: {abc:${abc}}`)
 })
