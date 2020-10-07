332c462b4589c11283ebbae76a56a5e3be541720
im-not-enemy <opkd0710@gmail.com>
2020-10-07 03:37:54 +0000

    [Sound] transpose(): 臨時記号を選択可能に

--- src/Entity/Sound.ts
+++ src/Entity/Sound.ts
@@ -1,8 +1,11 @@
 //概要: ABC記譜法の文字列
 //仕様: transpose(semitones:number): 
 //      半音上げ下げしたABC文字を返す
-//      元の音が調号なしの場合: 調号なし or フラットの音を返す
+//      元の音が調号なしの場合: 
+//          priolityの指定なし: フラットを返す
+//          priolityの指定あり: 指定された臨時記号で返す
 //      元の音がシャープの場合: シャープを返す
+//      元の音がフラットの場合: フラットを返す
 
 import Note from '@tonaljs/note'
 import {toMidi} from '@tonaljs/midi'
@@ -10,12 +13,14 @@ import abc from '@tonaljs/abc-notation'
 
 export default class Sound {
     constructor(public src:string){}
-    public transpose(seminotes:number){
+    public transpose(seminotes:number,priolity?:string){
         const before = toMidi(abc.abcToScientificNotation(this.src))
         if (before !== null){
-            const note = RegExp(/^\^/).test(this.src)
-            ? Note.fromMidiSharps(before + seminotes)
-            : Note.fromMidi(before + seminotes)
+            let note
+            if (RegExp(/^\^/).test(this.src)) note = Note.fromMidiSharps(before + seminotes)
+            else if (RegExp(/^_/).test(this.src)) note = Note.fromMidi(before + seminotes)
+            else if (priolity === 'sharp') note = Note.fromMidiSharps(before + seminotes)
+            else note = Note.fromMidi(before + seminotes)
             this.src = abc.scientificToAbcNotation(note)
         }
         return this
--- src/__tests__/Sound.test.ts
+++ src/__tests__/Sound.test.ts
@@ -5,7 +5,12 @@ test('transpose()',()=>{
     expect(new Sound('C').transpose(-1).toString()).toBe('B,')
     expect(new Sound('C').transpose(2).toString()).toBe('D')
     expect(new Sound('C').transpose(7).toString()).toBe('G')
+
     expect(new Sound('^C').transpose(7).toString()).toBe('^G')
+    expect(new Sound('B,').transpose(7).toString()).toBe('_G')
+    expect(new Sound('B,').transpose(7,'sharp').toString()).toBe('^F')
+    expect(new Sound('B,').transpose(7,'flat').toString()).toBe('_G')
+
     expect(new Sound('G').transpose(0).toString()).toBe('G')
     expect(new Sound('G').transpose(-1).toString()).toBe('_G')
     expect(new Sound('G').transpose(2).toString()).toBe('A')
